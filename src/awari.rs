806ed1ec7a4bd351e1417c5c32abc35668ed1815
peio <peio.borthelle@ens-lyon.fr>
2017-06-07 18:04:35 +0200

    moves implemented (sow, unsow, collect..)

--- src/awari.rs
+++ src/awari.rs
@@ -1,4 +1,6 @@
-use std::ops::Index;
+use std::ops::{Index,IndexMut};
+use std::fmt;
+use std::cmp;
 
 #[cfg(test)]
 use quickcheck::{Arbitrary,Gen};
@@ -6,20 +8,37 @@ use quickcheck::{Arbitrary,Gen};
 use utils;
 
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
 pub struct Board4([u8; 8]);
 
 
 impl Index<usize> for Board4 {
     type Output = u8;
     fn index(&self, i: usize) -> &u8 {
-        let &Board4(ref b) = self;
+        let Board4(ref b) = *self;
         &b[i]
     }
 }
 
+impl fmt::Debug for Board4 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "\n-----------\n")?;
+        write!(f, "{:2}{:3}{:3}{:3}\n", self[7], self[6], self[5], self[4])?;
+        write!(f, "{:2}{:3}{:3}{:3}\n", self[0], self[1], self[2], self[3])?;
+        write!(f, "-----------\n")
+    }
+}
 
 impl Board4 {
+    pub fn unwrap(self) -> [u8; 8] {
+        let Board4(b) = self;
+        b
+    }
+
+    pub fn iter_config(n: u8) -> impl Iterator<Item=Self> {
+        (0..65739375).map(Self::decode)
+    }
+
     pub fn encode(&self) -> u64 {
         let mut c = self[0] as u64;
         let mut g = utils::binom(1, c);
@@ -51,10 +70,83 @@ impl Board4 {
         b[7] = (n - s - c1) as u8;
         return Board4(*b);
     }
+
+    pub fn rotate(&mut self) {
+        let mut b = self.unwrap();
+        for i in 0..4 {
+            b.swap(i, i+4);
+        }
+    }
+
+    pub fn is_valid(&self, i: usize) -> bool {
+        let n = self[i];
+        if i >= 4 || n == 0 { return false; }
+        else {
+            let (q, r) = (n / 7, (n % 7) as usize);
+            let j = (i + r + 1) % 8;
+            let mut take = true;
+            for k in (4..8).rev() {
+                if k > j {
+                    if self[k] > q { return true; }
+                } else {
+                    take = take && (self[k] == 2 || self[k] == 3);
+                    if !take && self[k] > q+1 { return true; }
+                }
+            }
+            return false;
+        }
+    }
+
+    fn sow(&mut self, i: usize) -> usize {
+        assert!(i < 4, "pit index out of bounds");
+        assert!(self[i] > 0, "no seeds in pit");
+        let n = self[i];
+        let mut b = self.unwrap();
+        b[i] = 0;
+        let (q, r) = (n / 7, (n % 7) as usize);
+        for j in 1..r+1 {
+            b[(i+j) % 8] += q+1;
+        }
+        for j in r+1..8 {
+            b[(i+j) % 8] += q;
+        }
+        (i+r+1) % 8
+    }
+
+    fn unsow(&mut self, i: usize, n: u8) -> usize {
+        // compute the rightmost min cell on my side, left of i
+        let mut j = cmp::min(3, i);
+        while j > 0 && self[j] != 0 {
+            j -= 1;
+        }
+        assert!(self[j] == 0, "invalid unsowing");
+        let mut b = self.unwrap();
+        for k in 0..j {
+            assert!(b[k] >= n, "invalid unsowing");
+            b[k] -= n;
+        }
+        for k in j..i+1 {
+            assert!(b[k] >= n+1, "invalid unsowing");
+            b[k] -= n + 1;
+        }
+        for k in i+1..8 {
+            assert!(b[k] >= n, "invalid unsowing");
+            b[k] -= n;
+        }
+        j
+    }
+
+    fn collect(&self, i: usize) -> usize {
+        let mut j = i;
+        while j >= 4 && (self[j] == 2 || self[j] == 3) {
+            j -= 1;
+        }
+        j
+    }
 }
 
 
-//TODO: more uniform generator...
+//TODO: make the generator more uniform...
 #[cfg(test)]
 impl Arbitrary for Board4 {
     fn arbitrary<G: Gen>(g: &mut G) -> Board4 {
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![feature(plugin)]
+#![feature(conservative_impl_trait)]
+
 #![plugin(quickcheck_macros)]
 
 #[cfg(test)]
