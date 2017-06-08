1bc8551ef420841be3e5f69d401423ee0d624e01
peio <peio.borthelle@ens-lyon.fr>
2017-06-08 23:58:29 +0200

    change in coding (simpler, probably more efficient)

--- src/awari.rs
+++ src/awari.rs
@@ -1,14 +1,16 @@
 use std::ops::{Index,IndexMut};
-use std::fmt;
-use std::cmp;
+use std::fmt::{Debug,Formatter,Result};
+use std::cmp::min;
+use std::hash::{Hash,Hasher};
+use std::iter::Iterator;
 
 #[cfg(test)]
 use quickcheck::{Arbitrary,Gen};
 
-use utils;
+use utils::{binom,binom_maxinv};
 
 
-#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone, PartialEq, Eq)]
 pub struct Board4([u8; 8]);
 
 
@@ -20,8 +22,9 @@ impl Index<usize> for Board4 {
     }
 }
 
-impl fmt::Debug for Board4 {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+
+impl Debug for Board4 {
+    fn fmt(&self, f: &mut Formatter) -> Result {
         write!(f, "\n-----------\n")?;
         write!(f, "{:2}{:3}{:3}{:3}\n", self[7], self[6], self[5], self[4])?;
         write!(f, "{:2}{:3}{:3}{:3}\n", self[0], self[1], self[2], self[3])?;
@@ -29,46 +32,47 @@ impl fmt::Debug for Board4 {
     }
 }
 
+
+impl Hash for Board4 {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        state.write_usize(self.encode());
+    }
+}
+
+
 impl Board4 {
     pub fn unwrap(self) -> [u8; 8] {
         let Board4(b) = self;
-        b
+        return b;
     }
 
-    pub fn iter_config(n: u8) -> impl Iterator<Item=Self> {
-        (0..65739375).map(Self::decode)
+    pub fn iter_config(n: usize) -> impl Iterator<Item=Self> {
+        (binom(8, 7 + n)..binom(8, 8+n)).map(Board4::decode)
     }
 
-    pub fn encode(&self) -> u64 {
-        let mut c = self[0] as u64;
-        let mut g = utils::binom(1, c);
-        for i in 1..7 {
-            c += 1 + self[i] as u64;
-            g += utils::binom(i as u64 + 1, c);
+    pub fn encode(&self) -> usize {
+        let (mut n, mut c) = (0, 0);
+        for (i, &x) in self.unwrap().into_iter().enumerate() {
+            c += x as usize;
+            n += binom(i + 1, c + i);
         }
-        let n = (0..8).fold(0, |a, i| a + self[i]) as u64;
-        n + 25 * g
+        return n;
     }
 
-    pub fn decode(g : u64) -> Self {
-        let b = &mut [0; 8];
-        let n : u64 = g % 25;
-        let mut g : u64 = g / 25;
-
-        let mut s = 0;
-        let (mut c1, d) = utils::binom_maxinv(7, g);
-        g -= d;
-        for k in (0..6).rev() {
-            let (c0, d) = utils::binom_maxinv(k as u64 + 1, g);
-            g -= d;
-            let a = c1 - c0 - 1;
-            s += a;
-            b[k+1] = a as u8;
-            c1 = c0;
+    pub fn decode(n : usize) -> Self {
+        let mut n = n;
+        let mut brd = [0; 8];
+        for i in (0..8).rev() {
+            let (x, b) = binom_maxinv(i + 1, n);
+            n -= b;
+            brd[i] = x as u8;
         }
-        b[0] = c1 as u8;
-        b[7] = (n - s - c1) as u8;
-        return Board4(*b);
+        let mut c = brd[0];
+        for i in 1..8 {
+            brd[i] = c;
+            c += brd[i] + 1;
+        }
+        return Board4(brd);
     }
 
     pub fn rotate(&mut self) {
@@ -110,12 +114,12 @@ impl Board4 {
         for j in r+1..8 {
             b[(i+j) % 8] += q;
         }
-        (i+r+1) % 8
+        return (i+r+1) % 8;
     }
 
     fn unsow(&mut self, i: usize, n: u8) -> usize {
         // compute the rightmost min cell on my side, left of i
-        let mut j = cmp::min(3, i);
+        let mut j = min(3, i);
         while j > 0 && self[j] != 0 {
             j -= 1;
         }
@@ -133,36 +137,49 @@ impl Board4 {
             assert!(b[k] >= n, "invalid unsowing");
             b[k] -= n;
         }
-        j
+        return j;
     }
 
-    fn collect(&self, i: usize) -> usize {
+    fn collect(&mut self, i: usize) -> u8 {
         let mut j = i;
-        while j >= 4 && (self[j] == 2 || self[j] == 3) {
+        let mut n = 0;
+        let mut b = self.unwrap();
+        while j >= 4 && (b[j] == 2 || b[j] == 3) {
+            n += b[j];
+            b[j] = 0;
             j -= 1;
         }
-        j
+        return n;
+    }
+
+    pub fn play(&mut self, i: usize) -> u8 {
+        let j = self.sow(i);
+        let k = self.collect(j);
+        self.rotate();
+        return k;
     }
 }
 
 
-//TODO: make the generator more uniform...
 #[cfg(test)]
 impl Arbitrary for Board4 {
     fn arbitrary<G: Gen>(g: &mut G) -> Board4 {
-        let b = &mut [0; 8];
-        let mut s = 24u8;
-        for i in 0..8 {
-            let x = g.gen_range(0u8, s);
-            b[i] = x;
-            s -= x;
+        let mut b = [0; 8];
+        for i in 1..8 {
+            b[i] = g.gen_range(0u8, 24);
         }
-        Board4(*b)
+        b.sort();
+        for i in 0..7 {
+            b[i] = b[i+1] - b[i];
+        }
+        b[7] = 24 - b[7];
+        return Board4(b);
     }
 }
 
 
 #[quickcheck]
 fn coding_bijective(b: Board4) -> bool {
+    println!("{:?}\n{}\n{:?}", b, b.encode(), Board4::decode(b.encode()));
     b == Board4::decode(b.encode())
 }
