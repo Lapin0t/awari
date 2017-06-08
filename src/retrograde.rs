8a2a0bcbcd436861c2ddb0038dc58a0a3fc75651
peio <peio.borthelle@ens-lyon.fr>
2017-06-08 23:57:38 +0200

    RA WIP...

--- src/retrograde.rs
+++ src/retrograde.rs
@@ -1,21 +1,43 @@
-use awari;
+use awari::Board4;
 use std::collections::HashMap;
+use std::vec::Vec;
 
-enum State = {
-    Instable(i8, u8),
-    Stable(i8),
-}
+type State = (i8, u8);
+type Row = HashMap<Board4, State>;
 
-fn initialize(table: &[HashMap<awari::Board4, State>; 25], k: usize) -> HashMap<awari::Board4, State> {
-    for
-}
+pub fn analysis() -> Vec<Row> {
+    let mut table = [(); 65739375];
+    for n in 0..25 {
+        let mut row = Row::new();
 
-pub fn analysis() -> [HashMap<awari::Board4, State>; 25] {
-    let mut table: [HashMap<awari::Board4, State>; 25];
-    for k in 0..25 {
-        for
-        let mut curr = initialize(&table, k);
-        converge(&mut curr, &table, k);
-        table[k] = curr;
+        // initialization
+        for mut brd in Board4::iter_config(n) {
+            let mut st = (-24, 4);
+            for i in 0..4 {
+                if !brd.is_valid(i) {
+                    st.1 -= 1;
+                } else {
+                    let k = brd.play(i) as usize;
+                    if k > 0 {
+                        st.1 -= 1;
+                        st.0 = k as i8 - table[n - k].get(&brd).unwrap().0;
+                    }
+                }
+            }
+            row.insert(brd, st);
+        }
+
+        // convergence
+        for l in 0..n+1 {
+            for brd in Board4::iter_config(n) {
+                let &mut st = row.get_mut(&brd);
+                if st.1 == 0 || st.0 == n - l {
+                    propagate(&mut row, brd);
+                }
+            }
+        }
+
+        table[n] = row;
     }
+    return table;
 }
