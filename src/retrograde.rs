961cd11fc4ab0a80b2efaa4cadbe2b96caa79d1f
peio <peio.borthelle@ens-lyon.fr>
2017-06-15 18:12:16 +0200

    cleanup, WIP

--- src/retrograde.rs
+++ src/retrograde.rs
@@ -10,6 +10,7 @@ pub type Table = Box<[(i8, u8); 10518300]>;
 fn propagate(table: &mut Table, u: &Board4, score: i8, sat: i8) {
     let id = u.encode();
     let st = table[id];
+    println!("propagate: board: {}, score: {}, tbd: {}", id, st.0, st.1);
     if st.1 > 0 {
         table[id].1 -= 1;
         let s = max(st.0, -score);
@@ -21,37 +22,16 @@ fn propagate(table: &mut Table, u: &Board4, score: i8, sat: i8) {
         }
     } else {
         println!("ERROR: id={}; v-board={}, v-succ={}", id, st.0, score);
-        assert!(st.0 >= -score);
+       // assert!(st.0 >= -score);
     }
-   /*
-    let s_u = table[u.encode()];
-    if s_u.0 == sat || s_u.1 == 0 {
-        table[u.encode()].1 = 0;
-        for v in u.predecessors() {
-            let id = v.encode();
-            if table[id].1 != 0 {
-                //println!("foo");
-                table[id].0 = max(table[id].0, -s_u.0);
-                table[id].1 -= 1;
-                propagate(table, &v, sat);
-            } else {
-                //println!("id: {}, {}, {}",id,  table[id].0, -s_u.0);
-                // check we got something compatible
-                if table[id].0 < -s_u.0 {
-                    println!("incoherance: brd val={}, succ val={}", table[id].0, s_u.0);
-                }
-                //assert!(table[id].0 >= -s_u.0);
-            }
-        }
-    }*/
 }
 
 
 pub fn analysis() -> Table {
     let mut table = HEAP <- [(-24, 0); 10518300];
     //table[0] = (0, 0);
-    for n in 0..9 {
-        println!("debug: seed num {}", n);
+    for n in 0..3 {
+        println!("seed num {}", n);
 
         // initialization
         for u in Board4::iter_config(n) {
@@ -69,10 +49,12 @@ pub fn analysis() -> Table {
 
         // convergence
         for l in 0..n+1 {
+            println!("iteration {}", l);
             for u in Board4::iter_config(n) {
                 let id = u.encode();
                 let sat = (n - l) as i8;
                 let st = table[id];
+                println!("board: {}, score: {}, tbd: {}", id, st.0, st.1);
                 if st.0 == sat || st.1 == 0 {
                     table[id].1 = 0;
                     for v in u.predecessors() {
