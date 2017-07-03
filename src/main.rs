057e4c67bb14edf30ccd879e69ff870845b16e66
peio <peio.borthelle@ens-lyon.fr>
2017-07-03 19:15:04 +0200

    upgrade

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ extern crate awari;
 
 use awari::{PITS,SEEDS,NBOARDS};
 use awari::awari::Awari;
-use awari::retrograde::analysis;
+use awari::ra::{analyze,Storage};
+use awari::storage::NaiveRam;
 
 fn main() {
     println!("PITS={} SEEDS={} NBOARDS={}", PITS, SEEDS, NBOARDS);
-    let table = analysis();
-    for n in 0..25 {
+    let table = analyze::<NaiveRam>(SEEDS);
+    for n in 0..SEEDS+1 {
         println!("configurations with {} seeds", n);
         for u in Awari::iter_config(n) {
             let id = u.encode();
-            println!("{:10}: {}", id, table[id].0);
+            println!("{:10}: {}", id, table.get(id).value());
         }
     }
 }
