514da1d186229013d4d351fa6f38c552ddea1576
peio <peio.borthelle@ens-lyon.fr>
2017-07-05 15:37:12 +0200

    fix for new interface

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
+use std::env;
+use std::option::Option;
+
 extern crate awari;
 
 use awari::{PITS,SEEDS,NBOARDS};
 use awari::awari::Awari;
 use awari::ra::{analyze,Storage};
-use awari::storage::NaiveRam;
+use awari::models;
+
 
 fn main() {
     println!("PITS={} SEEDS={} NBOARDS={}", PITS, SEEDS, NBOARDS);
-    let table = analyze::<NaiveRam>(SEEDS);
-    for n in 0..SEEDS+1 {
+
+    let upto = match env::args().nth(1) {
+        Option::Some(n) => n.parse::<usize>().unwrap(),
+        Option::None => SEEDS
+    };
+
+    let table = analyze::<models::NaiveDisk>(upto);
+    for n in 0..upto+1 {
         println!("configurations with {} seeds", n);
         for u in Awari::iter_config(n) {
             let id = u.encode();
-            println!("{:10}: {}", id, table.get(id).value());
+            println!("{:10}: {}", id, table.value(id));
         }
     }
 }
