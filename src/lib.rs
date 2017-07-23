8dc10035a40c37382cb7bca752b68212d89165b8
peio <peio.borthelle@ens-lyon.fr>
2017-07-24 01:00:29 +0200

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -3,32 +3,17 @@
 #![cfg_attr(test, feature(test, plugin))]
 #![cfg_attr(test, plugin(quickcheck_macros))]
 
-#[cfg(test)]
-extern crate quickcheck;
-
-#[cfg(test)]
-extern crate test;
-
-#[cfg(test)]
-extern crate rand;
-
-#[macro_use]
-extern crate log;
-
-#[macro_use]
-extern crate lazy_static;
-
+#[cfg(test)] extern crate quickcheck;
+#[cfg(test)] extern crate test;
+#[cfg(test)] extern crate rand;
+#[macro_use] extern crate log;
 extern crate tempfile;
 
 
 pub mod utils;
 pub mod awari;
 pub mod ra;
-pub mod storage;
-pub mod models;
-
-#[cfg(test)]
-mod disk_bench;
+mod storage;
 
 // include constants (see `build.rs`)
 include!(concat!(env!("OUT_DIR"), "/size.rs"));
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,24 @@
 use std::env;
 use std::option::Option;
-use std::fs::File;
-use std::io::{BufWriter,Write};
-use std::mem::transmute;
 
+#[macro_use] extern crate log;
+extern crate env_logger;
 extern crate awari;
 
 use awari::{PITS,SEEDS,NBOARDS};
-use awari::awari::Awari;
-use awari::ra::analyze;
-use awari::models;
+use awari::ra::{Table,NaiveRAM};
 
 
 fn main() {
-    println!("PITS={} SEEDS={} NBOARDS={}", PITS, SEEDS, NBOARDS);
+    env_logger::init().unwrap();
+
+    info!("PITS={} SEEDS={} NBOARDS={}", PITS, SEEDS, NBOARDS);
 
     let upto = match env::args().nth(1) {
         Option::Some(n) => n.parse::<usize>().unwrap(),
         Option::None => SEEDS,
     };
 
-    let table = analyze::<models::NaiveRAM>(upto);
-
-    if !env::args().any(|x| x == "--quiet") {
-        let mut out = BufWriter::new(File::create("out").unwrap());
-        for n in 0..upto+1 {
-            //println!("configurations with {} seeds", n);
-            for (c, u) in Awari::iter_config(n) {
-                write!(&mut out, "{:10}: {}\n", c, table.index(c).value()).unwrap();
-            }
-        }
-        let mut out = BufWriter::new(File::create("access").unwrap());
-        let tmp = table.stats();
-        for &i in tmp.iter() {
-            unsafe {
-                out.write(&transmute::<usize,[u8;8]>(i)).unwrap();
-            }
-        }
-    }
-    println!("memory accesses: {}", table.stats().len());
+    let mut table = NaiveRAM::new(".");
+    table.build(upto);
 }
