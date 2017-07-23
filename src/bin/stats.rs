bfed80e6866411896f479c6e9067e80dd179b610
peio <peio.borthelle@ens-lyon.fr>
2017-07-24 01:02:12 +0200

    much more useful stats: estimated memory accesses

--- src/bin/stats.rs
+++ src/bin/stats.rs
@@ -7,19 +7,45 @@ use std::env;
 
 use rand::{Rng,thread_rng};
 
-use awari::NBOARDS;
+use awari::{FPITS,SEEDS};
 use awari::awari::Awari;
+use awari::utils::n_boards;
 
 
-fn rand_awari<R: Rng>(gen: &mut R) -> Awari {
-    Awari::decode(gen.gen_range(0, NBOARDS))
+
+fn rand_awari<R: Rng>(g: &mut R, n: u8) -> Awari {
+    let mut b: Awari = Default::default();
+
+    b[0] = 0;
+    for i in 1..FPITS {
+        b[i] = g.gen_range(0, n);
+    }
+    b.sort();
+    for i in 0..FPITS-1 {
+        b[i] = b[i+1] - b[i];
+    }
+    b[FPITS-1] = n - b[FPITS-1];
+    return b;
 }
 
-fn mean_gain<R: Rng>(gen: &mut R, n: usize) -> f64 {
-    let mut x = 0;
-    let mut cnt = 0;
+
+fn mean<R,F,T>(gen: &mut R, seeds: u8, n: usize, mut f: F) -> f64
+    where R: Rng,
+          F: FnMut(Awari) -> T,
+          T: Into<f64> {
+    let mut tot = 0.0;
     for _ in 0..n {
-        let u = rand_awari(gen);
+        tot += f(rand_awari(gen, seeds)).into();
+    }
+    return tot / (n as f64);
+        
+}
+
+fn mean_gain<R: Rng>(gen: &mut R, seeds: u8, n: usize) -> f64 {
+    let mut x = 0;
+    let mut cnt = 0usize;
+    for _ in 0..n {
+        let u = rand_awari(gen, seeds);
         for (_, k) in u.successors() {
             cnt += 1;
             x += k as usize;
@@ -29,44 +55,28 @@ fn mean_gain<R: Rng>(gen: &mut R, n: usize) -> f64 {
 }
 
 
-fn mean_nz_move<R: Rng>(gen: &mut R, n: usize) -> f64 {
-    let mut x = 0;
-    for _ in 0..n {
-        let u = rand_awari(gen);
-        for (_, k) in u.successors() {
-            if k > 0 {
-                x += 1;
-            }
-        }
-    }
-    return (x as f64) / (n as f64);
-}
-
-fn mean_moves<R: Rng>(gen: &mut R, n: usize) -> f64 {
-    let mut x = 0;
-    for _ in 0..n {
-        let u = rand_awari(gen);
-        x += u.successors().len();
-    }
-    return (x as f64) / (n as f64);
-}
-
-fn mean_predecessors<R: Rng>(gen: &mut R, n: usize) -> f64 {
-    let mut x = 0;
-    for _ in 0..n {
-        let u = rand_awari(gen);
-        x += u.predecessors().len();
-    }
-    return (x as f64) / (n as f64);
-}
-
-
 fn main() {
-    let n = env::args().nth(1).unwrap().parse::<usize>().unwrap();
-    println!("{} samples", n);
+    let niters = env::args().nth(1).unwrap().parse::<usize>().unwrap();
+    println!("{} samples per measure", niters);
+    println!("====== ====== ====== ======");
+    println!("  gain  moves  nz-mv  bk-mv");
+    println!("====== ====== ====== ======");
     let mut gen = thread_rng();
-    println!("mean gain: {}", mean_gain(&mut gen, n));
-    println!("mean moves: {}", mean_moves(&mut gen, n));
-    println!("mean non-zero moves: {}", mean_nz_move(&mut gen, n));
-    println!("mean back-moves: {}", mean_predecessors(&mut gen, n));
+    let mut cost = 0.0;
+
+    for n in 1..SEEDS+1 {
+        let s = n as u8;
+        let g = mean_gain(&mut gen, s, niters);
+        let mv = mean(&mut gen, s, niters, |u| u.successors().len() as f64);
+        let nzm = mean(&mut gen, s, niters, |u| u.successors().into_iter()
+                .filter(|&(_, k)| k > 0).count() as f64);
+        let bm = mean(&mut gen, s, niters, |u| u.predecessors().len() as f64);
+
+        if n != SEEDS-1 {
+            cost += (2.0 + ((n+1)/2) as f64 + bm + nzm) * (n_boards(n) as f64);
+        }
+        println!("{:6.4} {:6.4} {:6.4} {:6.4}", g, mv, nzm, bm);
+    }
+    println!("====== ====== ====== ======");
+    println!("estimated memory accesses: {}", cost);
 }
