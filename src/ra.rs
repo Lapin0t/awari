2ad4435b191a2ec919fdda186f84c8236f3853c4
peio <peio.borthelle@ens-lyon.fr>
2017-08-08 17:15:19 +0200

    wrapped the table building in a driver struct

--- src/ra.rs
+++ src/ra.rs
@@ -3,11 +3,12 @@ use std::option::Option;
 use std::ops::IndexMut;
 
 use core::nonzero::NonZero;
+use slog::Logger;
 
 use SEEDS;
 use awari::Awari;
 
-pub use storage::{NaiveRAM,MMaped};
+pub use ::storage::{NaiveRAM,MMaped};
 
 
 #[derive(Eq,PartialEq)]
@@ -63,7 +64,9 @@ impl State {
     #[inline]
     pub fn try_stabilize(&mut self, sat_lvl: i8) -> Option<i8> {
         if sat_lvl == 0 {
-            // little hack: if sat_lvl == 0 this is the last branch
+            // little hack: if sat_lvl == 0 this is the last step, so we can
+            // unconditionnaly set any instable state to stable with score 0
+            // (without propagating)
             if self.nsuc != Option::None {
                 self.val = 0;
                 self.nsuc = Option::None;
@@ -90,78 +93,85 @@ pub trait Table: IndexMut<usize, Output=State> {
 }
 
 
-/// Update the given state with the final score of one of its successors.
-/// Propagate it recursively whenever it flips the:w
-// state to a final score.
-fn propagate<T: Table>(table: &mut T, u: Awari, up: i8, sat_lvl: i8) {
-    let mut stack = vec![(u, up)];
-    while let Some((u, a)) = stack.pop() {
-        if let Some(b) = table[u.encode()].update(a, sat_lvl) {
-            debug_assert!(-sat_lvl <= b && b <= sat_lvl);
-            // if update changed to final value, propagate further
-            for v in u.predecessors() {
-                stack.push((v, b));
-            }
-        }
-    }
+pub struct Driver<T: Table> {
+    pub table: T,
+    pub logger: Logger,
 }
 
-fn iteration<T: Table>(table: &mut T, n: usize) {
-    info!("start of iteration {}", n);
-    table.pre_hook(n);
 
-    info!("initialization");
-    for (c, u) in Awari::iter_config(n) {
-        let (mut score, mut nsucc) = (-(n as i8), 0);
-        for (v, k) in u.successors() {
-            if k > 0 {
-                score = max(score, k as i8 - table[v.encode()].val);
-            }
-            nsucc += 1;
-        }
-        table.insert(c, State::new(score, nsucc));
-    }
-
-    info!("convergence");
-    for l in 0..(n+1)/2 {
-        info!("step {}", 2*l);
-        let sat_lvl = (n - 2*l) as i8;
-        for (c, u) in Awari::iter_config(n) {
-            // yup, temporary lifetimes have struck again..
-            if let Some(x) = { let ref mut tmp = table[c];
-                               tmp.try_stabilize(sat_lvl) } {
-                debug_assert!(-sat_lvl <= x && x <= sat_lvl);
+impl<T: Table> Driver<T> {
+    /// Update the given state with the final score of one of its successors.
+    /// Propagate it recursively whenever it flips the state to a final score.
+    fn propagate(&mut self, u: Awari, up: i8, sat_lvl: i8) {
+        let mut stack = vec![(u, up)];
+        while let Some((u, a)) = stack.pop() {
+            if let Some(b) = self.table[u.encode()].update(a, sat_lvl) {
+                debug_assert!(-sat_lvl <= b && b <= sat_lvl);
+                // if update changed to final value, propagate further
                 for v in u.predecessors() {
-                    propagate(table, v, x, sat_lvl);
+                    stack.push((v, b));
                 }
             }
         }
     }
-    if n & 1 == 0 {
-        info!("step {}", n);
-        for (c, _) in Awari::iter_config(n) {
-            let ref mut tmp = table[c];
-            if tmp.nsuc != Option::None {
-                tmp.val = 0;
-                tmp.nsuc = Option::None;
+
+    fn iteration(&mut self, n: usize) {
+        info!(self.logger, "iter start"; "iter" => n);
+        self.table.pre_hook(n);
+
+        info!(self.logger, "initialization"; "iter" => n);
+        for (c, u) in Awari::iter_config(n) {
+            let (mut score, mut nsucc) = (-(n as i8), 0);
+            for (v, k) in u.successors() {
+                if k > 0 {
+                    score = max(score, k as i8 - self.table[v.encode()].val);
+                }
+                nsucc += 1;
+            }
+            self.table.insert(c, State::new(score, nsucc));
+        }
+
+        info!(self.logger, "convergence"; "iter" => n);
+        for l in 0..(n+1)/2 {
+            info!(self.logger, "step start"; "iter" => n, "step" => 2*l);
+            let sat_lvl = (n - 2*l) as i8;
+            for (c, u) in Awari::iter_config(n) {
+                // yup, temporary lifetimes have struck again..
+                if let Some(x) = { let ref mut tmp = self.table[c];
+                                   tmp.try_stabilize(sat_lvl) } {
+                    debug_assert!(-sat_lvl <= x && x <= sat_lvl);
+                    for v in u.predecessors() {
+                        self.propagate(v, x, sat_lvl);
+                    }
+                }
             }
         }
+        if n & 1 == 0 {
+            info!(self.logger, "step start"; "iter" => n, "step" => n);
+            for (c, _) in Awari::iter_config(n) {
+                let ref mut tmp = self.table[c];
+                if tmp.nsuc != Option::None {
+                    tmp.val = 0;
+                    tmp.nsuc = Option::None;
+                }
+            }
+        }
+        info!(self.logger, "iter end"; "iter" => n);
     }
-}
 
+    /// Construct the optimal score table! Yay!
+    pub fn run(&mut self) {
+        // first iteration
+        self.table.pre_hook(0);
+        self.table.insert(0, State { val: 0, nsuc: Option::None });
+        self.table.post_hook(0);
 
-/// Construct the optimal score table! Yay!
-pub fn build<T: Table>(table: &mut T) {
-    // first iteration
-    table.pre_hook(0);
-    table.insert(0, State { val: 0, nsuc: Option::None });
-    table.post_hook(0);
+        // don't compute the second to last iteration
+        for n in 1..SEEDS-1 {
+            self.iteration(n);
+        }
+        self.iteration(SEEDS);
 
-    // don't compute the second to last iteration
-    for n in 1..SEEDS-1 {
-        iteration(table, n);
+        self.table.finish_hook();
     }
-    iteration(table, SEEDS);
-
-    table.finish_hook();
 }
